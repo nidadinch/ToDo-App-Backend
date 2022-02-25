f02e92283918d8b02c4b9636f2078095bbadb15a
Nida Dinç <20160808047@ogr.akdeniz.edu.tr>
2022-02-25 15:17:55 +0300

    add empty items test

--- repository/item_repository_test.go
+++ repository/item_repository_test.go
@@ -9,26 +9,39 @@ import (
 	"backend/repository"
 )
 
-func Test_GetAll(t *testing.T) {
-	items := []*model.Item{
-		{
-			Id:   1,
-			Text: "practice go",
-		},
-		{
-			Id:   2,
-			Text: "buy some cheese",
-		},
-	}
-	// Create repository
-	itemsRepo := repository.NewItemRepository()
-	// Append items to repository
-	for _, s := range items {
-		itemsRepo.NewItem(s)
-	}
-	want := items
-	got, err := itemsRepo.GetAllItems()
-	assert.Nil(t, err)
-	assert.Equal(t, got, want)
+func Test_GetAllItems(t *testing.T) {
+	t.Run("should return all items correctly", func(t *testing.T) {
+		items := []*model.Item{
+			{
+				Id:   1,
+				Text: "practice go",
+			},
+			{
+				Id:   2,
+				Text: "buy some cheese",
+			},
+		}
+		// Create repository
+		itemsRepo := repository.NewItemRepository()
+		// Append items to repository
+		for _, s := range items {
+			itemsRepo.NewItem(s)
+		}
+		want := items
+		got, err := itemsRepo.GetAllItems()
+		assert.Nil(t, err)
+		assert.Equal(t, got, want)
 
+	})
+
+	t.Run("should return empty items if inMemoryDB is empty", func(t *testing.T) {
+		items := []*model.Item{}
+
+		itemsRepo := repository.NewItemRepository()
+
+		want := items
+		got, err := itemsRepo.GetAllItems()
+		assert.Nil(t, err)
+		assert.Equal(t, got, want)
+	})
 }
